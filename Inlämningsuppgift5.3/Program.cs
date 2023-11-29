5ba62447fa1c4069f947888576c66486a82a1e7c
Keli06001 <keli06001@utb.vaxjo.se>
2023-11-29 08:33:06 +0100

    Add project files.

---
+++ Inlämningsuppgift5.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inlämningsuppgift5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heltalArray = new int[3];
+
+            for (int i = 0; i < heltalArray.Length; i++)
+            {
+                Console.Write($"Ange heltal {i + 1}: ");
+                heltalArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int summa = heltalArray[0] + heltalArray[1] + heltalArray[2];
+
+            Console.WriteLine($"Summan av de tre heltalen är: {summa}");
+
+        }
+    }
+}
